1d093f9dcfec8ccb8ad812b491a32fad0deb67fa
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-01-31 22:38:06 +0000

    added typing

--- minstore.pyi
+++ minstore.pyi
@@ -19,43 +19,65 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from __future__ import division, absolute_import, with_statement, print_function, unicode_literals
-from renpy.compat import PY2, basestring, bchr, bord, chr, open, pystr, range, round, str, tobytes, unicode # *
-
+from __future__ import (
+    division,
+    absolute_import,
+    with_statement,
+    print_function,
+    unicode_literals,
+)
+from renpy.compat import (
+    PY2,
+    basestring,
+    bchr,
+    bord,
+    chr,
+    open,
+    pystr,
+    range,
+    round,
+    str,
+    tobytes,
+    unicode,
+)  # *
 
 xrange = range
 
-unicode = str # @ReservedAssignment
+unicode = str  # @ReservedAssignment
 
 python_list = _list = list
 python_dict = _dict = dict
-python_object = _object = object # type: ignore
+python_object = _object = object  # type: ignore
 python_set = _set = set
 
 _type = type
 
 from renpy.revertable import RevertableList as __renpy__list__
-list = __renpy__list__ # @ReservedAssignment
+
+list = __renpy__list__  # @ReservedAssignment
 
 from renpy.revertable import RevertableDict as __renpy__dict__
-dict = __renpy__dict__ # @ReservedAssignment
+
+dict = __renpy__dict__  # @ReservedAssignment
 
 from renpy.revertable import RevertableDefaultDict as __renpy_defaultdict__
+
 defaultdict = __renpy_defaultdict__
 
 from renpy.revertable import RevertableSet as __renpy__set__
-set = __renpy__set__ # @ReservedAssignment
+
+set = __renpy__set__  # @ReservedAssignment
 Set = __renpy__set__
 
-from renpy.revertable import RevertableObject as object # @UnusedImport
+from renpy.revertable import RevertableObject as object  # @UnusedImport
 
-from renpy.revertable import revertable_range as range # @UnusedImport
-from renpy.revertable import revertable_sorted as sorted # @UnusedImport
+from renpy.revertable import revertable_range as range  # @UnusedImport
+from renpy.revertable import revertable_sorted as sorted  # @UnusedImport
 
 from renpy.revertable import MultiRevertable
 
-import renpy.ui as ui # @UnusedImport
-from renpy.translation import translate_string as __ # @UnusedImport
+import renpy.ui as ui  # @UnusedImport
+from renpy.translation import translate_string as __  # @UnusedImport
 
 from renpy.python import store_eval as eval
 
@@ -63,10 +85,10 @@ from renpy.display.core import absolute
 from renpy.atl import position
 
 import renpy
+
 globals()["renpy"] = renpy.exports
 
 _print = print
-
 
 def print(*args, **kwargs):
     """
@@ -76,21 +98,10 @@ def print(*args, **kwargs):
     at the start of the next statement, so that it can't be rolled past.
     """
 
-    renpy.game.context().force_checkpoint = True # type: ignore
+    renpy.game.context().force_checkpoint = True  # type: ignore
     _print(*args, **kwargs)
 
-
-def _(s):
-    """
-    :undocumented: Documented directly in the .rst.
-
-    Flags a string as translatable, and returns it immediately. The string
-    will be translated when displayed by the text displayable.
-    """
-
-    return s
-
-
+def _(s: str) -> str: ...
 def _p(s):
     '''
     :doc: underscore_p
@@ -126,7 +137,7 @@ def _p(s):
 
     import re
 
-    lines = [ i.strip() for i in s.split("\n") ]
+    lines = [i.strip() for i in s.split("\n")]
 
     if lines and not lines[0]:
         lines.pop(0)
@@ -135,16 +146,16 @@ def _p(s):
         lines.pop()
 
     rv = ""
-    para = [ ]
+    para = []
 
     for l in lines:
         if not l:
             rv += " ".join(para) + "\n\n"
-            para = [ ]
-        elif re.search(r'\{p[^}]*\}$', l):
+            para = []
+        elif re.search(r"\{p[^}]*\}$", l):
             para.append(l)
             rv += " ".join(para)
-            para = [ ]
+            para = []
         else:
             para.append(l)
 
@@ -156,51 +167,52 @@ def input(*args, **kwargs):
     :undocumented:
     """
 
-    raise Exception("The Python input and raw_input functions do not work with Ren'Py. Please use the renpy.input function instead.")
+    raise Exception(
+        "The Python input and raw_input functions do not work with Ren'Py. Please use the renpy.input function instead."
+    )
 
 raw_input = input
 
-
 __all__ = [
-    'PY2',
-    'Set',
-    '_',
-    '__',
-    '__renpy__dict__',
-    '__renpy__list__',
-    '__renpy__set__',
-    '_dict',
-    '_list',
-    '_object',
-    '_p',
-    '_print',
-    '_set',
-    '_type',
-    'absolute',
-    'basestring',
-    'bchr',
-    'bord',
-    'dict',
-    'eval',
-    'input',
-    'list',
-    'object',
-    'open',
-    'position',
-    'print',
-    'python_dict',
-    'python_list',
-    'python_object',
-    'python_set',
-    'range',
-    'raw_input',
-    'set',
-    'sorted',
-    'str',
-    'tobytes',
-    'ui',
-    'unicode',
+    "PY2",
+    "Set",
+    "_",
+    "__",
+    "__renpy__dict__",
+    "__renpy__list__",
+    "__renpy__set__",
+    "_dict",
+    "_list",
+    "_object",
+    "_p",
+    "_print",
+    "_set",
+    "_type",
+    "absolute",
+    "basestring",
+    "bchr",
+    "bord",
+    "dict",
+    "eval",
+    "input",
+    "list",
+    "object",
+    "open",
+    "position",
+    "print",
+    "python_dict",
+    "python_list",
+    "python_object",
+    "python_set",
+    "range",
+    "raw_input",
+    "set",
+    "sorted",
+    "str",
+    "tobytes",
+    "ui",
+    "unicode",
 ]
 
 if PY2:
-    __all__ = [ bytes(i) for i in __all__ ] # type: ignore
+    __all__ = [bytes(i) for i in __all__]  # type: ignore
